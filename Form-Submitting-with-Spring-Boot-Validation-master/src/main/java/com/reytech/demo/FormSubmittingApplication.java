a4a9ca8549e3faa7677ea5ee76a2e6e033486d5e
Noor <Noor@LAPTOP-L4R1EAVA>
2021-06-17 11:05:11 +0530

    noor

---
+++ Form-Submitting-with-Spring-Boot-Validation-master/src/main/java/com/reytech/demo/FormSubmittingApplication.java
@@ -0,0 +1,13 @@
+package com.reytech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmittingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSubmittingApplication.class, args);
+	}
+
+}
